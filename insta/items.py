e6696c7d75024125a7322d202f512a46a3c6a4cd
Dmitriy <dmitriy.v.sizov@gmail.com>
2021-09-09 20:06:30 +0300

    Lesson 8

---
+++ insta/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstaItem(scrapy.Item):
+    _id = scrapy.Field()
+    user_id = scrapy.Field()
+    user_fullname = scrapy.Field()
+    subuser_type = scrapy.Field()
+    subuser_id = scrapy.Field()
+    subuser_fullname = scrapy.Field()
+    subuser_link_to_pic = scrapy.Field()
